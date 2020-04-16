53922f287f8ecb84d266417826d3a5ccd23f566f
daichihasegawa0923 <punyachi@yahoo.co.jp>
2020-04-17 00:23:57 +0900

    くそ動画を追加

--- Cloud/Assets/Code/Start/StartManager.cs
+++ Cloud/Assets/Code/Start/StartManager.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
     [SerializeField] private GameObject _cmaeraBody;
-
+    [SerializeField] private AudioSource _audioSouce;
+    [SerializeField] private Animator[] _animator;
     // Update is called once per frame
     void Update()
     {
-        var spin = _cmaeraBody.transform.eulerAngles;
-        spin.y += 1.0f;
-        _cmaeraBody.transform.eulerAngles = spin;
+        _animator.All(ani => { ani.SetInteger("time", (int)_audioSouce.time); return true; });
     }
 
     public void GoToControlPanel(string sceneName)
