c8de2e491ed31ce478ca926209e2327d806496a0
daichihasegawa0923 <punyachi@yahoo.co.jp>
2020-04-25 01:49:01 +0900

    UI追加

---
+++ Cloud/Assets/Code/UI/MenuOnGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuOnGame : MonoBehaviour
+{
+    [SerializeField] protected GameObject _panel;
+    [SerializeField] protected Button _openClose;
+    [SerializeField] protected Slider _soundVolumeSlider;
+
+    [SerializeField] private float _panelMove = 400;
+
+    private void Awake()
+    {
+        _soundVolumeSlider.onValueChanged.AddListener(value => 
+        {
+            foreach (var audioSouce in FindObjectsOfType<AudioSource>())
+                audioSouce.volume = value;
+        });
+
+        _openClose.onClick.AddListener(() =>
+        {
+            var buttonText = _openClose.gameObject.transform.Find("Text").gameObject.GetComponent<Text>();
+            var panelPosition = _panel.transform.position;
+            if (buttonText.text == "Open")
+            {
+                panelPosition.x += _panelMove;
+                buttonText.text = "Close";
+            }
+            else
+            {
+                panelPosition.x -= _panelMove;
+                buttonText.text = "Open";
+            }
+
+            _panel.transform.position = panelPosition;
+        });
+    }
+}
